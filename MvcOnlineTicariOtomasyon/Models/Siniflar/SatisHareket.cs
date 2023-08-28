0e9fae3aa1c81c03473690b9f38a6267340d3769
Şule Celep <120751470+sulecelep@users.noreply.github.com>
2023-08-28 21:18:48 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/SatisHareket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHareket
+    {
+        [Key]
+        public int SatisHareketid { get; set; }
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public ICollection<Urun> Uruns { get; set; }
+        public ICollection<Cari> Caris { get; set; }
+        public ICollection<Personel> Personels { get; set; }
+
+    }
+}
