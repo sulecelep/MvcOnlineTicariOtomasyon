ec90c6a5fd6532c45fa21004dcbd77e22526587a
Şule Celep <120751470+sulecelep@users.noreply.github.com>
2023-09-06 21:02:15 +0300

    66 Completed

---
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,93 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+
+        Context context = new Context();
+        // GET: Satis
+        public ActionResult Index()
+        {
+            var values= context.SatisHarekets.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> urunler = (from x in context.Uruns.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text= x.UrunAd,
+                                             Value= x.Urunid.ToString()
+                                         }).ToList();
+            
+
+            List<SelectListItem> cariler = (from x in context.Carilers.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.CarilerAd+" "+x.CarilerSoyad,
+                                                Value = x.Carilerid.ToString()
+                                            }).ToList();
+
+            List<SelectListItem> personeller = (from x in context.Personels.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                                Value = x.Personelid.ToString()
+                                            }).ToList();
+
+
+            ViewBag.Urunler = urunler;
+            ViewBag.Cariler = cariler;
+            ViewBag.Personeller = personeller;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket satisHareket)
+        {
+            satisHareket.Tarih=DateTime.Parse(DateTime.Now.ToShortDateString());
+            context.SatisHarekets.Add(satisHareket);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult SatisGuncelle(int id)
+        {
+            List<SelectListItem> urunler = (from x in context.Uruns.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.UrunAd,
+                                                Value = x.Urunid.ToString()
+                                            }).ToList();
+
+
+            List<SelectListItem> cariler = (from x in context.Carilers.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.CarilerAd + " " + x.CarilerSoyad,
+                                                Value = x.Carilerid.ToString()
+                                            }).ToList();
+
+            List<SelectListItem> personeller = (from x in context.Personels.ToList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                                    Value = x.Personelid.ToString()
+                                                }).ToList();
+
+
+            ViewBag.Urunler = urunler;
+            ViewBag.Cariler = cariler;
+            ViewBag.Personeller = personeller;
+            var value = context.SatisHarekets.Find(id);
+            
+            return View(value);
+        }
+    }
+}
